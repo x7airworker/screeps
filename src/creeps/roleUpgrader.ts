c9670fc2e35a9bf69ef71505617cb9bbe195b54a
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-02-27 22:16:20 +0100

    roleUpgrader: optimize pathfinding

--- src/creeps/roleUpgrader.ts
+++ src/creeps/roleUpgrader.ts
@@ -1,3 +1,5 @@
+import globals from "core/globals";
+
 export default {
   run(creep: Creep): void {
     let shouldUpgrade = false;
@@ -12,17 +14,17 @@ export default {
     if (shouldUpgrade) {
       if (creep.room.controller) {
         if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE)
-          creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: "#3333" } });
+          creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: "#3333ff" } });
       }
     } else {
-      const source: Source | null = creep.pos.findClosestByPath(FIND_SOURCES);
+      const source: Source | null | undefined = creep.room.controller?.pos.findClosestByRange(FIND_SOURCES);
 
       if (source instanceof Source) {
         if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(source, { visualizePathStyle: { stroke: "#3333" } });
+          creep.moveTo(source, { visualizePathStyle: { stroke: "#333333" } });
         }
       } else {
-        creep.say("No sources found!");
+        creep.say(globals.MSG_ERR_SOURCE_NOT_FOUND);
       }
     }
   }
