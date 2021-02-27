d2816a200c0714e7e5a442ebd5aa7153a3140f18
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-02-27 22:14:53 +0100

    globals: add error message for no sources

--- src/core/globals.ts
+++ src/core/globals.ts
@@ -1,5 +1,6 @@
 export default {
   ROLE_HARVESTER: "harvester",
   ROLE_BUILDER: "builder",
-  ROLE_UPGRADER: "upgrader"
+  ROLE_UPGRADER: "upgrader",
+  MSG_ERR_SOURCE_NOT_FOUND: "No sources found!"
 };
