f3da98a6865d9328036ea38772cd4e8007cb8a6d
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-03-01 19:15:09 +0100

    globals: change err msg

--- src/core/globals.ts
+++ src/core/globals.ts
@@ -2,7 +2,7 @@ export default {
   ROLE_HARVESTER: "harvester",
   ROLE_BUILDER: "builder",
   ROLE_UPGRADER: "upgrader",
-  MSG_ERR_SOURCE_NOT_FOUND: "No sources found!",
+  MSG_ERR_NOT_FOUND: "Nothing to work on or harvest found!",
   MSG_HARVEST: "H",
   MSG_WORKING: "W",
 };
