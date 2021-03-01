76c243db575a1c02cfdf58fdd6857e30b74e8f4b
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-03-01 19:23:13 +0100

    globals: shorten idle err msg

--- src/core/globals.ts
+++ src/core/globals.ts
@@ -2,7 +2,7 @@ export default {
   ROLE_HARVESTER: "harvester",
   ROLE_BUILDER: "builder",
   ROLE_UPGRADER: "upgrader",
-  MSG_ERR_NOT_FOUND: "Nothing to work on or harvest found!",
+  MSG_ERR_NOT_FOUND: "Idling",
   MSG_HARVEST: "H",
   MSG_WORKING: "W",
 };
