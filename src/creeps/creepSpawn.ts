35494a123a2d3cbbf91d47b048b7a0e8e0d504ce
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-03-01 19:15:39 +0100

    creepSpawn: optimize and fix

--- src/creeps/creepSpawn.ts
+++ src/creeps/creepSpawn.ts
@@ -6,24 +6,22 @@ export default function (): void {
   if (
     _(Game.creeps)
       .filter({ memory: { role: globals.ROLE_HARVESTER } })
-      .size() < 2
+      .size() < 3
   )
     Game.spawns.Spawn1.spawnCreep([WORK, CARRY, CARRY, MOVE], `Harvester_${uuid()}`, {
-      memory: { role: globals.ROLE_HARVESTER },
+      memory: { role: globals.ROLE_HARVESTER, working: false },
     });
-
   // Spawn new upgraders
-  if (
+  else if (
     _(Game.creeps)
       .filter({ memory: { role: globals.ROLE_UPGRADER } })
-      .size() < 5
+      .size() < 3
   )
     Game.spawns.Spawn1.spawnCreep([WORK, WORK, CARRY, MOVE], `Upgrader_${uuid()}`, {
       memory: { role: globals.ROLE_UPGRADER, working: false },
     });
-
   // Spawn new builders
-  if (
+  else if (
     _(Game.creeps)
       .filter({ memory: { role: globals.ROLE_BUILDER } })
       .size() < 10
