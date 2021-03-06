fde0130db25f6d1ada57f168fef8abb1c1577364
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-03-06 16:27:50 +0100

    creepFarm: let creeps collect energy droplets and resources from tombstones

--- src/creeps/creepFarm.ts
+++ src/creeps/creepFarm.ts
@@ -1,9 +1,31 @@
 import globals from "core/globals";
 
 export default function (creep: Creep, color: string): void {
+  const droplets: Resource<ResourceConstant>[] | undefined = creep.room.find(FIND_DROPPED_RESOURCES, {
+    filter: d => d.resourceType === RESOURCE_ENERGY,
+  });
+
+  const tombstones: Tombstone[] | undefined = creep.room.find(FIND_TOMBSTONES, {
+    filter: t => t.creep.store.getCapacity(RESOURCE_ENERGY) > 0,
+  });
+
   const source: Source | null = creep.pos.findClosestByPath(FIND_SOURCES, {
     filter: s => s.energy > creep.store.getFreeCapacity(),
   });
+
+  if (tombstones.length) {
+    creep.say(globals.MSG_HARVEST);
+    if (creep.withdraw(tombstones[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(tombstones[0]);
+    }
+  }
+
+  if (droplets.length) {
+    creep.say(globals.MSG_HARVEST);
+    if (creep.pickup(droplets[0]) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(droplets[0]);
+    }
+  }
 
   if (!source) {
     creep.say(globals.MSG_ERR_NOT_FOUND);
