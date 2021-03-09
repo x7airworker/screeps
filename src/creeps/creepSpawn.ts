93f7d99365c9570fd37a530d3ad7c2b104bcdb3f
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-03-09 18:28:19 +0100

    gameSpawn: add cost of base creep body; return if not enough energy to spawn basic creep; log new body information (debug)

--- src/creeps/creepSpawn.ts
+++ src/creeps/creepSpawn.ts
@@ -2,19 +2,31 @@ import globals from "../core/globals";
 import uuid from "../core/uuid";
 
 const bodyBase: BodyPartConstant[] = [WORK, CARRY, MOVE];
+let costBase = 0;
+for (const part of bodyBase) {
+  costBase += BODYPART_COST[part];
+}
 
 function composeBody(): BodyPartConstant[] {
   const energyCapacityAvailable = Game.rooms.W13N33.energyAvailable;
   const bodyNew = bodyBase;
-  let pushCost = 0;
+  let pushCost = costBase;
   for (const part of bodyBase) {
     pushCost += BODYPART_COST[part];
   }
+
+  console.log("-------------------------------\nInitial values:\n-------------------------------");
+  console.log(`Base body: ${bodyBase.toString()}`);
+  console.log(`Push cost: ${pushCost}`);
+  console.log(`New body: ${bodyNew.toString()}`);
 
   while (energyCapacityAvailable > pushCost) {
     for (const part of bodyBase) {
       pushCost += BODYPART_COST[part];
       if (energyCapacityAvailable <= pushCost) {
+        console.log(
+          `Early return due to push costs being too high to maintan pushing new body parts\nNew body: ${bodyNew.toString()}`,
+        );
         return bodyNew;
       }
       bodyNew.push(part);
@@ -25,10 +37,16 @@ function composeBody(): BodyPartConstant[] {
     console.error("Couldn't compose new body!");
   }
 
+  console.log(`New body is being returned with the following values: ${bodyNew.toString()}`);
+
   return bodyNew;
 }
 
-export default function (): void {
+export default function (spawn: StructureSpawn): void {
+  // Check if energy capacity is high enough to spawn a base creep
+  if (spawn.room.energyAvailable < costBase) {
+    return;
+  }
   if (
     _(Game.creeps)
       .filter({ memory: { role: globals.ROLE_HARVESTER } })
