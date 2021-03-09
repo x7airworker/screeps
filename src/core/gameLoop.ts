9e8bec01a00f04216c608ee6ca0ebec0c44d0297
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-03-09 18:27:03 +0100

    gameLoop: loop over owned spawns and check if they're in the spawning state before attempting to spawn new creeps

--- src/core/gameLoop.ts
+++ src/core/gameLoop.ts
@@ -2,8 +2,12 @@ import creepSpawn from "creeps/creepSpawn";
 import creepWork from "creeps/creepWork";
 
 export default function (): void {
-  // Spawn all the creeps!
-  creepSpawn();
+  // Loop over all own spawns and check if they're spawning something allready
+  for (const spawn in Game.spawns) {
+    if (!Game.spawns[spawn].spawning) {
+      creepSpawn(Game.spawns[spawn]);
+    }
+  }
 
   // Let the creeps work
   creepWork();
