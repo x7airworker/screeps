65ccd253853dd38e0063f59adf95779b4f55ab74
x7airworker <jake@jakes-co.de>
2021-08-12 14:28:14 +0200

    More upgrader logic

--- src/creeps/jobs/upgrader.ts
+++ src/creeps/jobs/upgrader.ts
@@ -5,20 +5,15 @@ import creepFarm from "../creepFarm";
 export default function (creep: Creep) {
     creepFailsafe(creep);
     if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
-      creep.memory.working = false;
-    }
-    if (!creep.memory.working && creep.store.getFreeCapacity() === 0) {
-      creep.memory.working = true;
+        creep.memory.working = false;
     }
 
-    if (!creep.memory.working) {
-      creepFarm(creep, globals.COLOR_UPGRADER);
-    } else {
-      if (creep.room.controller) {
+    if (creep.room.controller) {
         creep.say(globals.MSG_WORKING);
         if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: globals.COLOR_UPGRADER } });
+            creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: globals.COLOR_UPGRADER } });
+        } else {
+            creep.memory.working = true;
         }
-      }
     }
 }
