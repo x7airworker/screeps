3789ee94052a2822a1b33984ceb76a21b07e2a21
x7airworker <jake@jakes-co.de>
2021-08-12 21:44:53 +0200

    Added the basic tutorial logic, I have to think about this later

--- src/creeps/jobs/builder.ts
+++ src/creeps/jobs/builder.ts
@@ -4,31 +4,24 @@ import creepFarm from "../creepFarm";
 
 export default function (creep: Creep) {
     creepFailsafe(creep);
-    if (creep.memory.working && (creep.store[RESOURCE_ENERGY] === 0 || creep.store.getFreeCapacity() === 0)) {
-      creep.memory.working = false;
+
+    if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
+        creep.memory.working = false;
     }
 
-    if (!creep.memory.working) {
-      creepFarm(creep, globals.COLOR_BUILDER);
+    if (!creep.memory.working && creep.store.getFreeCapacity() == 0) {
+        creep.memory.working = true;
+    }
+
+    if (creep.memory.working) {
+        const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+        if (targets.length) {
+            if (creep.build(targets[0]) == ERR_NOT_IN_RANGE)
+                creep.moveTo(targets[0]);
+        }
     } else {
-      const structure: ConstructionSite | null = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-
-      const repairables: AnyStructure[] | undefined = creep.room
-        .find(FIND_STRUCTURES, {
-          filter: c => c.hits < c.hitsMax,
-        })
-        .sort((a, b) => a.hits - b.hits);
-
-      if (structure) {
-        creep.say(globals.MSG_WORKING);
-        if (creep.build(structure) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(structure, { visualizePathStyle: { stroke: globals.COLOR_BUILDER } });
-        }
-      } else if (repairables.length) {
-        creep.say(globals.MSG_WORKING);
-        if (creep.repair(repairables[0]) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(repairables[0], { visualizePathStyle: { stroke: globals.COLOR_BUILDER } });
-        }
-      }
+        const sources = creep.room.find(FIND_SOURCES);
+        if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
+            creep.moveTo(sources[0]);
     }
 }
--- src/creeps/jobs/upgrader.ts
+++ src/creeps/jobs/upgrader.ts
@@ -1,19 +1,25 @@
 import globals from "core/globals";
 import creepFailsafe from "../creepFailsafe";
-import creepFarm from "../creepFarm";
 
 export default function (creep: Creep) {
     creepFailsafe(creep);
     if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
         creep.memory.working = false;
     }
+    if (!creep.memory.working && creep.store.getFreeCapacity() === 0) {
+        creep.memory.working = true;
+    }
 
     if (creep.room.controller) {
         creep.say(globals.MSG_WORKING);
-        if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
-            creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: globals.COLOR_UPGRADER } });
+        if (creep.memory.working) {
+            if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: globals.COLOR_UPGRADER } });
+            }
         } else {
-            creep.memory.working = true;
+            const sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
+                creep.moveTo(sources[0]);
         }
     }
 }
